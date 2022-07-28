1afd63c1360a1f3b822727d2f6f1400657d70bc1
Andrey Iljasov <a_iljasov@mail.ru>
2022-07-28 11:51:02 +0300

    Input negative numbers in productCount allowed

--- src/Main.java
+++ src/Main.java
@@ -43,17 +43,21 @@ class Main {
                     continue;
                 }
                 countProduct = Integer.parseInt(strCountProduct);
-                if (countProduct <= 0) {
-                    System.out.println("Некорректное количество");
-                    continue;
-                }
+
             } catch (NumberFormatException e) {
                 System.out.println("Введены некорректные данные");
                 continue;
             }
 
-            sumProducts += price[indexProduct] * countProduct;
-            basket[indexProduct] += countProduct;
+            if (countProduct == 0) {
+
+                basket[indexProduct] = 0;
+
+            } else {
+
+                sumProducts += price[indexProduct] * countProduct;
+                basket[indexProduct] += countProduct;
+            }
 
 
         }
