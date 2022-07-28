800e077d7978abe8a1efff3dd49a89931eaa322b
Andrey Iljasov <a_iljasov@mail.ru>
2022-07-28 13:35:14 +0300

    Reformat code, str.81 break added

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,6 @@ class Main {
                 continue;
             }
 
-
             String strIndexProduct = String.valueOf(stringsInput[0]);
             String strCountProduct = String.valueOf(stringsInput[1]);
             int indexProduct;
@@ -37,19 +36,21 @@ class Main {
 
             try {
 
-
                 indexProduct = Integer.parseInt(strIndexProduct) - 1;
+
                 if ((indexProduct < 0) || (indexProduct + 1 > products.length)) {
+
                     System.out.println("Некорректное значение выбранного продукта");
                     continue;
+
                 }
+
                 countProduct = Integer.parseInt(strCountProduct);
 
             } catch (NumberFormatException e) {
                 System.out.println("Введены некорректные данные");
                 continue;
             }
-
 
             if (countProduct == 0) {
 
@@ -60,36 +61,36 @@ class Main {
                 basket[indexProduct] += countProduct;
             }
 
-
-
-
-
         }
         System.out.println("Ваша корзина:");
 
         for (int i = 0; i < basket.length; i++) {
+
             boolean discount = false;
+
             if (basket[i] > 0) {
+
                 for (int j = 0; j < productsStock.length; j++) {
+
                     if (products[i].equals(productsStock[j])) {
+
                         discount = true;
                         int actionSum = ((basket[i] - basket[i] % 3) * price[i] * 2) / 3 + basket[i] % 3 * price[i];
                         sumProducts += actionSum;
                         System.out.println(products[i] + " " + basket[i] + " шт " + price[i] + " руб/шт " + actionSum + " рублей в сумме");
+                        break;
                     }
-
-
                 }
+
                 if (!discount) {
+
                     System.out.println(products[i] + " " + basket[i] + " шт " + price[i] + " руб/шт " + (basket[i] * price[i]) + " рублей в сумме");
                     sumProducts += basket[i] * price[i];
                 }
             }
-
         }
+
         System.out.println("Итого: " + sumProducts);
-
-
     }
 
 }
